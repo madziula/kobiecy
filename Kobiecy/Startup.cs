7ce69e5c051d7e7578b34a7adc49f5ac0dc2a014
madziula <madziula_@onet.pl>
2015-04-10 13:55:30 +0200

    lol

---
+++ Kobiecy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kobiecy.Startup))]
+namespace Kobiecy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
